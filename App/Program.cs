aa7b0083f566c4767ec77b499c2a87f973a60515
PaulNonatomic <paul@nonatomic.co.uk>
2023-09-30 11:22:34 +0100

    Completed the basic project creation flow

--- App/Program.cs
+++ App/Program.cs
@@ -1,4 +1,5 @@
-﻿using UnityQuickStart.App.Github;
+﻿using System.Diagnostics;
+using UnityQuickStart.App.Github;
 using UnityQuickStart.App.HelpSystem;
 using UnityQuickStart.App.IO;
 using UnityQuickStart.App.Settings;
@@ -13,6 +14,10 @@ public class Program
 	private static UserSettings _userSettings;
 	private static CommandLineArgs _cmdArgs;
 	private static string _projectPath;
+	private static string _projectName;
+	private static bool _localRepoCreated;
+	private static bool _githubRepoCreated;
+	private static bool _unityProjectCreated;
 
 	static void Main(string[] args)
 	{
@@ -43,14 +48,53 @@ public class Program
 		CreateLocalRepo();
 		CreateRemoteRepo();
 		CreateUnityProject();
+		OpenUnityProject();
 	}
 
 	private static void SetProjectName()
 	{
-		//@Todo get the project name !!
+		var currentDirectoryName = Path.GetFileName(Environment.CurrentDirectory);
 		
-		// Output.WriteHint($"Press enter to use current directory name: {Environment.CurrentDirectory}");
-		// Path.GetFileName
+		Output.WriteHint($"Press enter to use current directory name: {currentDirectoryName}");
+		var projectName = UserInput.GetString(
+			"Enter the project name: ",
+			required: false
+		);
+		
+		if (string.IsNullOrEmpty(projectName))
+		{
+			projectName = currentDirectoryName;
+		}
+		else
+		{
+			//check if the project name matches the current directory name
+			if (currentDirectoryName != projectName)
+			{
+				var createSubDir = UserInput.GetYesNo($"Would you like to create a {projectName} sub directory?");
+				var newProjectPath = Path.Combine(_projectPath, projectName);
+				
+				if (createSubDir)
+				{
+					try
+					{
+						Directory.CreateDirectory(newProjectPath);
+						
+						_projectPath = newProjectPath;
+						Directory.SetCurrentDirectory(_projectPath);
+						Output.WriteSuccessWithTick($"Ok project path updated to: {_projectPath}");
+					}
+					catch (Exception e)
+					{
+						Output.WriteError(e.Message);
+						SetProjectName();
+						return;
+					}
+				}
+			}
+		}
+		
+		_projectName = projectName;
+		Output.WriteSuccessWithTick($"Ok project name set: {projectName}");
 	}
 
 	private static void SetProjectPath()
@@ -92,6 +136,7 @@ public class Program
 		}
 
 		_projectPath = projectPath;
+		Directory.SetCurrentDirectory(_projectPath);
 		Output.WriteSuccessWithTick($"Ok project path set: {projectPath}");
 	}
 
@@ -199,16 +244,214 @@ public class Program
 
 	private static void CreateUnityProject()
 	{
-		
+		try
+		{
+			var cts = new CancellationTokenSource();
+			var spinnerTask = Task.Run(() => Spinner(cts.Token));
+			var psi = new ProcessStartInfo
+			{
+				FileName = GetPathToUnityVersion(),
+				Arguments = @$"-batchmode -quit -createProject {_projectPath}",
+				RedirectStandardOutput = true,
+				RedirectStandardError = true,
+				UseShellExecute = false,
+				CreateNoWindow = true,
+				Verb = "runas"
+			};
+			
+			using (var process = new Process())
+			{
+				process.StartInfo = psi;
+				process.Start();
+				process.WaitForExit();
+
+				cts.Cancel();
+				spinnerTask.Wait();
+				
+				var output = process.StandardOutput.ReadToEnd();
+				var error = process.StandardError.ReadToEnd();
+
+				if (process.ExitCode == 0)
+				{
+					_unityProjectCreated = true;
+					Output.WriteSuccessWithTick($"Ok Unity {_userSettings.GetUnityVersion()} project created at {_projectPath}");
+				}
+				else
+				{
+					_unityProjectCreated = false;
+					Output.WriteError($"Unity project creation failed: {error}");
+				}
+			}
+		}
+		catch (Exception ex)
+		{
+			_unityProjectCreated = false;
+			Output.WriteError($"Unity project creation failed: {ex.Message}");
+		}
 	}
 
+	private static string GetPathToUnityVersion()
+	{
+		return Path.Combine(_userSettings.GetUnityInstallPath(), 
+			_userSettings.GetUnityVersion(), 
+			"Editor",
+			"Unity.exe");
+	}
+
+	private static void OpenUnityProject()
+	{
+		var createRepo = UserInput.GetYesNo($"Would you like to open the Unity project at {_projectPath}:");
+		if (!createRepo)
+		{
+			Output.WriteSuccessWithTick($"Ok skip opening the project");
+			return;
+		}
+		
+		try
+		{
+			var process = new Process
+			{
+				StartInfo = new ProcessStartInfo
+				{
+					FileName = GetPathToUnityVersion(),
+					Arguments = $"-projectPath {_projectPath}",
+					RedirectStandardOutput = true,
+					RedirectStandardError = true,
+					UseShellExecute = false,
+					CreateNoWindow = true
+				}
+			};
+
+			process.Start();
+
+			var output = process.StandardOutput.ReadToEnd();
+			var error = process.StandardError.ReadToEnd();
+			process.WaitForExit();
+			
+			if (process.ExitCode == 0)
+			{
+				Output.WriteSuccessWithTick($"Ok Unity {_userSettings.GetUnityVersion()} project opened at {_projectPath}");
+			}
+			else
+			{
+				Output.WriteError($"Opening Unity project at {_projectPath} failed: {error}");
+			}
+		}
+		catch (Exception ex)
+		{
+			Output.WriteError($"Opening Unity project at {_projectPath} failed: {ex.Message}");
+		}
+	}
+	
 	private static void CreateRemoteRepo()
 	{
+		if (!_localRepoCreated) return;
 		
+		var createRepo = UserInput.GetYesNo($"Would you like to connect your local repo to a github repo:");
+		if (!createRepo)
+		{
+			Output.WriteSuccessWithTick($"Ok skipping github repo");
+			return;
+		}
+		
+		try
+		{
+			var process = new Process
+			{
+				StartInfo = new ProcessStartInfo
+				{
+					FileName = "gh",
+					Arguments = $"repo create {_projectName} --private --source {_projectPath}",
+					RedirectStandardOutput = true,
+					RedirectStandardError = true,
+					UseShellExecute = false,
+					CreateNoWindow = true
+				}
+			};
+
+			process.Start();
+
+			var output = process.StandardOutput.ReadToEnd();
+			var error = process.StandardError.ReadToEnd();
+			process.WaitForExit();
+			
+			if (process.ExitCode == 0)
+			{
+				_githubRepoCreated = true;
+				Output.WriteSuccessWithTick($"Ok Github repo {_projectName} created");
+			}
+			else
+			{
+				_githubRepoCreated = false;
+				Output.WriteError($"Github repo creation failed: {error}");
+			}
+		}
+		catch (Exception ex)
+		{
+			_githubRepoCreated = false;
+			Output.WriteError($"Github repo creation failed: {ex.Message}");
+		}
 	}
 
 	private static void CreateLocalRepo()
 	{
+		var createRepo = UserInput.GetYesNo($"Would you like to create a local git repo:");
+		if (!createRepo)
+		{
+			Output.WriteSuccessWithTick($"Ok skipping local repo");
+			return;
+		}
 		
+		try
+		{
+			var process = new Process
+			{
+				StartInfo = new ProcessStartInfo
+				{
+					FileName = "git",
+					Arguments = "init",
+					WorkingDirectory = _projectPath,
+					RedirectStandardOutput = true,
+					RedirectStandardError = true,
+					UseShellExecute = false,
+					CreateNoWindow = true
+				}
+			};
+
+			process.Start();
+
+			var output = process.StandardOutput.ReadToEnd();
+			var error = process.StandardError.ReadToEnd();
+
+			process.WaitForExit();
+
+			if (process.ExitCode == 0)
+			{
+				_localRepoCreated = true;
+				Output.WriteSuccessWithTick($"Ok local repo created in {_projectPath}");
+			}
+			else
+			{
+				_localRepoCreated = false;
+				Output.WriteError($"Repo creation failed: {error}");
+			}
+		}
+		catch (Exception ex)
+		{
+			_localRepoCreated = false;
+			Output.WriteError($"Repo creation failed: {ex.Message}");
+		}
+	}
+	
+	private static void Spinner(CancellationToken token)
+	{
+		char[] spinner = new char[] { '|', '/', '-', '\\' };
+		int i = 0;
+		while (!token.IsCancellationRequested)
+		{
+			Console.SetCursorPosition(0, 0);
+			Console.Write($"Processing {spinner[i++ % 4]}");
+			Thread.Sleep(200);
+		}
 	}
 }
